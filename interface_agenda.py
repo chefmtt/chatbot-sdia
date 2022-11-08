dbc406c8ca3c5c9601f02d5f5848eb4699b9477c
Clémentine Misiak <clementine.misiak@etu.unistra.fr>
2022-11-08 23:08:25 +0100

    couleurs

--- interface_agenda.py
+++ interface_agenda.py
@@ -9,38 +9,48 @@ import agenda
 # INTERFACE
 
 def print_agenda() :
-    
+
+
+    def couleur(i):
+        liste_couleur = ['red', 'green', 'yellow', 'blue', 'black']
+        return liste_couleur[i]
+
+
     def callback1():
         global week
         week = 45
         gui.destroy()
+
 
     def callback2():
         global week
         week = 46
         gui.destroy()
 
+
     def callback3():
         global week
         week = 47
         gui.destroy()
 
+
     gui = Tk()
     v = IntVar
-    gui.geometry('200x100')  
-    btn1 = Button(gui, text = "45", command = callback1)
+    gui.geometry('200x100')
+    btn1 = Button(gui, text="45", command=callback1)
     btn1.pack()
-    btn2 = Button(gui, text = "46", command = callback2)
+    btn2 = Button(gui, text="46", command=callback2)
     btn2.pack()
-    btn3 = Button(gui, text = "47", command = callback3)
+    btn3 = Button(gui, text="47", command=callback3)
     btn3.pack()
     gui.mainloop()
 
     # tableau qui affiche les données
+
+
     class Table:
 
-     
-        def __init__(self,gui,week): 
+        def __init__(self, gui, week):
 
             # données utilisées
             cal = agenda.Calendar("calendar")
@@ -54,49 +64,49 @@ def print_agenda() :
             print(total_rows)
             print(total_columns)
 
-            list_jours = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
-        
+            list_jours = ['Lundi', 'Mardi', 'Mercredi',
+                      'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+
             # code pour créer la table
             for i in range(total_rows):
                 for j in range(total_columns):
 
-                    if i == 0 :
-                        if j != 0 :
-                            self.e = Entry(gui, width = 10, fg = 'blue',
-                                    font = ('Arial',10,'bold'))
-                 
-                            self.e.grid(row=i, column = j)
+                    if i == 0:
+                        if j != 0:
+                            self.e = Entry(gui, width=10, fg='blue',
+                                       font=('Arial', 10, 'bold'))
+
+                            self.e.grid(row=i, column=j)
                             self.e.insert(END, list_jours[j-1])
-                        else :
+                        else:
                             self.e = Entry(gui, width=10, fg='black',
-                                    font  =('Arial',10))
-                 
-                            self.e.grid(row = i, column = j)
+                                       font=('Arial', 10))
+
+                            self.e.grid(row=i, column=j)
                             self.e.insert(END, 'Horaire')
 
-                    elif j == 0 :
-                        if i != 0 :
-                            self.e = Entry(gui, width = 10, fg = 'blue',
-                                    font = ('Arial',10,'bold'))
-                 
+                    elif j == 0:
+                        if i != 0:
+                            self.e = Entry(gui, width=10, fg='blue',
+                                       font=('Arial', 10, 'bold'))
+
                             self.e.grid(row=i, column=j)
                             self.e.insert(END, str(i-1) + ':00')
 
-                    else :
-                        self.e = Entry(gui, width=10, fg='black',
-                                    font=('Arial',10, 'bold'))
-                 
+                    else:
+                        indice = int(lst[i-2][j-2])
+
+                        self.e = Entry(gui, width=10, fg=couleur(indice),
+                                   font=('Arial', 10, 'bold'))
+
                         self.e.grid(row=i, column=j)
 
-                        indice = lst[i-2][j-2]
-
-                        if indice == 0 :
+                        if indice == 0:
                             self.e.insert(END, ' ')
-                    
-                        else :
-                            self.e.insert(END, documents_print[indice])
 
-  
+                        else:
+                            self.e.insert(END, documents_print[indice-1])
+
 
     gui = Tk()
     t = Table(gui, week)
